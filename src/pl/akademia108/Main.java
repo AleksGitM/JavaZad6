e778022203a94697e6c0bdec31fbbef66b41060d
Aleksander M <aleksander.michalski222@gmail.com>
2021-12-23 05:58:37 +0100

    poprawa wcięć

--- src/pl/akademia108/Main.java
+++ src/pl/akademia108/Main.java
@@ -3,16 +3,18 @@ package pl.akademia108;
 public class Main {
 
     public static void main(String[] args) {
-	byte a = 127;
-    byte b = 2;
-    short dodawanie = (short) (a + b);
-    byte odejmowanie = (byte) (a - b);
-    float dzielenie = (float) (a / b);
-    short mnozenie = (short) (a * b);
-        System.out.println("a wynosi:" + a);
-        System.out.println("a+2:" + dodawanie);
-        System.out.println("a-2:" + odejmowanie);
-        System.out.println("a/2:" + dzielenie);
-        System.out.println("a*2:" + mnozenie);
+        byte a = 127;
+        byte b = 2;
+
+            short dodawanie = (short) (a + b);
+            byte odejmowanie = (byte) (a - b);
+            float dzielenie = (float) (a / b);
+            short mnozenie = (short) (a * b);
+
+                System.out.println("a wynosi:" + a);
+                System.out.println("a+2:" + dodawanie);
+                System.out.println("a-2:" + odejmowanie);
+                System.out.println("a/2:" + dzielenie);
+                System.out.println("a*2:" + mnozenie);
     }
 }
